b168bf698904d389c77ea09c682696dfd70265e9
AC-Santiago <acostasantiago311124578@gmail.com>
2024-04-23 14:05:32 -0500

    Navegacion (desarrollo)

--- app/src/main/java/com/example/qta/PrimeraPagina.kt
+++ app/src/main/java/com/example/qta/PrimeraPagina.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import androidx.navigation.fragment.findNavController
 
 class PrimeraPagina : Fragment() {
 
@@ -17,6 +19,15 @@ class PrimeraPagina : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_primera_pagina, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val buttonEmpezar = view.findViewById<Button>(R.id.buttoninical)
+        buttonEmpezar.setOnClickListener{
+            findNavController().navigate(R.id.action_primeraPagina_to_inicioSesion)
+        }
     }
 
 
