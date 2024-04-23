2f87bd111caed9fd92a27bbbc633ead1e920e9c9
Sebastianmjk <mojicasebastian67@gmail.com>
2024-04-23 12:39:32 -0500

    Dos palabras

--- app/src/main/java/com/example/qta/DosPalabras.kt
+++ app/src/main/java/com/example/qta/DosPalabras.kt
@@ -13,10 +13,10 @@ private const val ARG_PARAM2 = "param2"
 
 /**
  * A simple [Fragment] subclass.
- * Use the [primera_pagina.newInstance] factory method to
+ * Use the [DosPalabras.newInstance] factory method to
  * create an instance of this fragment.
  */
-class primera_pagina : Fragment() {
+class DosPalabras : Fragment() {
     // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
@@ -34,7 +34,7 @@ class primera_pagina : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_primera_pagina, container, false)
+        return inflater.inflate(R.layout.fragment_dos_palabras, container, false)
     }
 
     companion object {
@@ -44,12 +44,12 @@ class primera_pagina : Fragment() {
          *
          * @param param1 Parameter 1.
          * @param param2 Parameter 2.
-         * @return A new instance of fragment primera_pagina.
+         * @return A new instance of fragment dos_palabras.
          */
         // TODO: Rename and change types and number of parameters
         @JvmStatic
         fun newInstance(param1: String, param2: String) =
-            primera_pagina().apply {
+            DosPalabras().apply {
                 arguments = Bundle().apply {
                     putString(ARG_PARAM1, param1)
                     putString(ARG_PARAM2, param2)
---
+++ app/src/main/java/com/example/qta/PrimeraPagina.kt
@@ -0,0 +1,23 @@
+package com.example.qta
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class PrimeraPagina : Fragment() {
+
+
+
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_primera_pagina, container, false)
+    }
+
+
+}
