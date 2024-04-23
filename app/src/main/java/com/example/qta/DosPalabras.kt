676f3cef5b7b7d3ff0899ae3ce03a992bbc47819
Sebastianmjk <mojicasebastian67@gmail.com>
2024-04-23 14:23:29 -0500

    Navegacion

--- app/src/main/java/com/example/qta/DosPalabras.kt
+++ app/src/main/java/com/example/qta/DosPalabras.kt
@@ -5,29 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [DosPalabras.newInstance] factory method to
- * create an instance of this fragment.
- */
 class DosPalabras : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        arguments?.let {
-            param1 = it.getString(ARG_PARAM1)
-            param2 = it.getString(ARG_PARAM2)
-        }
-    }
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
@@ -37,23 +21,23 @@ class DosPalabras : Fragment() {
         return inflater.inflate(R.layout.fragment_dos_palabras, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment dos_palabras.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            DosPalabras().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val imagendosmitades = view.findViewById<ImageView>(R.id.imageView2Mitades)
+        imagendosmitades.setOnClickListener {
+            findNavController().navigate(R.id.action_dosPalabras_to_dosMitades)
+        }
+
+        val imagenquitar = view.findViewById<ImageView>(R.id.imageViewQuitar)
+        imagenquitar.setOnClickListener {
+            findNavController().navigate(R.id.action_dosPalabras_to_last)
+
+        }
+
+        val imagenx = view.findViewById<ImageView>(R.id.imageView6)
+        imagenx.setOnClickListener {
+            findNavController().navigate(R.id.action_dosPalabras_to_inicioSesion)
+
+        }
     }
 }
--- app/src/main/java/com/example/qta/last.kt
+++ app/src/main/java/com/example/qta/last.kt
@@ -5,22 +5,11 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.navigation.fragment.findNavController
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [last.newInstance] factory method to
- * create an instance of this fragment.
- */
 class last : Fragment() {
-    // TODO: Rename and change types of parameters
-    private var param1: String? = null
-    private var param2: String? = null
-
 
 
     override fun onCreateView(
@@ -31,5 +20,24 @@ class last : Fragment() {
         return inflater.inflate(R.layout.fragment_last, container, false)
     }
 
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val imagendosmitades = view.findViewById<ImageView>(R.id.imageView11)
+        imagendosmitades.setOnClickListener {
+            findNavController().navigate(R.id.action_last_to_dosMitades)
+        }
+
+        val imagendospalabras = view.findViewById<ImageView>(R.id.imageView12)
+        imagendospalabras.setOnClickListener {
+            findNavController().navigate(R.id.action_last_to_dosPalabras)
+
+        }
+
+        val imagenx = view.findViewById<ImageView>(R.id.imageView6)
+        imagenx.setOnClickListener {
+            findNavController().navigate(R.id.action_last_to_inicioSesion)
+
+        }
+    }
 
 }
